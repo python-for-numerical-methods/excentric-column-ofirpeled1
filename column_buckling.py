bd1a03c3da8b71d3f61b3346b73059ad86bdb0b1
ofirpeled1 <ofirpe2@ac.sce.ac.il>
2026-05-12 17:10:48 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -1,17 +1,19 @@
 import numpy as np
 from scipy.optimize import bisect
 
-def find_critical_load(L, E, A, r, c, sigma_allow):
-
+def find_critical_load(L, E, A, r, c, e, sigma_allow):
     def f(P):
-
+        if P <= 0:
+            return -sigma_allow
+        
         angle = (L / (2 * r)) * np.sqrt(P / (E * A))
-
-        sigma_max = (P / A) * (1 + ((c * r) / (r**2)) * (1 / np.cos(angle)))
-
+        
+        # sec(x) = 1 / cos(x)
+        sigma_max = (P / A) * (1 + (e * c / (r**2)) * (1 / np.cos(angle)))
+        
         return sigma_max - sigma_allow
 
-    P_euler = (np.pi**2 * E * A * r**2) / (L**2)
-
-    return float(bisect(f, 0.01, P_euler * 0.99, xtol=1e-4))
+    # חסם עליון מבוסס על עומס אוילר התיאורטי
+    p_euler = (np.pi***2 * E * (A * r****2)) / (L***2)
     
+    return float(bisect(f, 0.01, p_euler * 0.99, xtol=1e-4))
